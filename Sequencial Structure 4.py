50f60fd5cf3920a4ba9fdcb12812f901fe25c585
Guaposo <joannpaixao@hotmail.com>
2022-02-01 17:35:01 -0300

    Create Sequencial Structure 4.py

---
+++ Sequencial Structure 4.py
@@ -0,0 +1,11 @@
+# Estrutura Sequencial 4
+x1 = input("Digite a primeira nota: ")
+a = int(x1)
+x2 = input("Digite a segunda nota: ")
+b = int(x2)
+x3 = input("Digite a terceira nota: ")
+c = int(x3)
+x4 = input("Digite a quarta nota: ")
+d = int(x4)
+media = (a+b+c+d)/4
+print("A média é ", media)
